af5b65db042bd6596ea1d88654a28a2e5d92e9a6
tigerfarm <tigerfarm@gmail.com>
2018-10-17 17:16:38 -0700

    updates

--- php/voiceConferenceEndSid.php
+++ php/voiceConferenceEndSid.php
@@ -3,15 +3,15 @@ if ($argc === 1 ) {
     echo "0";
     return;
 }
-$theConference = $argv[1];
+$theConferenceSid = $argv[1];
 
 require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
 
 use Twilio\Rest\Client;
 
 $twilio = new Client(getenv("ACCOUNT_SID"), getenv('AUTH_TOKEN'));
-echo "++ End Conference SID: " . $theConference . "\xA";
-$conference = $twilio->conferences($theConference)
+echo "++ End Conference SID: " . $theConferenceSid . "\xA";
+$conference = $twilio->conferences($theConferenceSid)
                      ->update(array("status" => "completed"));
 echo "++ Ended.\xA";
 ?>
