0bdc1131caa029fbb98b16d132d799b45b21b71b
tigerfarm <tigerfarm@gmail.com>
2019-02-22 14:57:03 -0800

    updates

---
+++ php/notifiySend_4.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
+use Twilio\Rest\Client;
+$twilio = new Client(getenv('ACCOUNT_SID'), getenv('AUTH_TOKEN'));
+$serviceSid = getenv('NOTIFY_SERVICE_SID');
+echo "+ NOTIFY_SERVICE_SID: " . $serviceSid . "\xA";
+echo "+ Notification to phone number: " . getenv('PHONE_NUMBER_2') . ", " . getenv('PHONE_NUMBER_3') . "\xA";
+$notification = $twilio->notify->services($serviceSid)
+    ->notifications->create([
+        "toBinding" => [
+            '{"binding_type":"sms", "address":"' . getenv('PHONE_NUMBER_2') . '"}',
+            '{"binding_type":"sms", "address":"' . getenv('PHONE_NUMBER_3') . '"}'
+        ],
+        "body" => "Hello Bob"
+    ]);
+
+echo "+ Notification SID: " . $notification->sid . "\xA";
+?>
