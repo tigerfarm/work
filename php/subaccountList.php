d7cd6ad6f38662a1b1a671420ee7658e0bb79354
tigerfarm <tigerfarm@gmail.com>
2019-03-13 13:01:38 -0700

    updates

---
+++ php/subaccountList.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
+use Twilio\Rest\Client;
+$twilio = new Client(getenv('ACCOUNT_SID'), getenv('AUTH_TOKEN'));
+// $accounts = $twilio->api->v2010->accounts->read();
+// echo "+ List all subaccounts for: ", getenv('ACCOUNT_SID'), "\xA";
+// foreach ($accounts as $record) {
+//     echo "++ account SID: ", $record->sid, " ", $record->friendlyName, "\xA";
+// }
+$accounts = $twilio->api->v2010->accounts->read(array("Status" => "active"));
+echo "+ List all active subaccounts for: ", getenv('ACCOUNT_SID'), "\xA";
+foreach ($accounts as $record) {
+    echo "++ account SID: ", $record->sid, " ", $record->friendlyName, "\xA";
+}
+echo "+ End of list.\xA";
+?>
