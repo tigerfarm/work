7d4483c1f88b5c85f9edb493dd807b306779d94d
tigerfarm <tigerfarm@gmail.com>
2019-04-17 10:41:37 -0700

    updates

--- php/voiceMakeCall-SayText.php
+++ php/voiceMakeCall-SayText.php
@@ -5,24 +5,24 @@ use Twilio\Rest\Client;
 $client = new Client(getenv('ACCOUNT_SID'), getenv('AUTH_TOKEN'));
 $callFrom = getenv('PHONE_NUMBER_1');
 // $callTo = getenv('PHONE_NUMBER_2');
-$callTo = '+34652308361';
+$callTo = getenv('PHONE_NUMBER_HOME');
 //  TwiML Bin: The Machine, Say poem: https://handler.twilio.com/twiml/EH50ee33d250f3302d012d63f6b7e6b1c4
 //  TwiML Bin: The Machine, Dial Johnathan: https://handler.twilio.com/twiml/EH60153b1250bb2eee3aee934a72123780
 //  TwiML Bin: The Machine, Say call from Twilio support: https://handler.twilio.com/twiml/EH10bd59478178c043b058b96142c69e06
-//  Function: The Machine, Say Polly: https://obedient-machine-3163.twil.io/saypolly
+//  Function: The Machine, Say Polly: https://" . getenv('FUNCTIONS_HOST') . "/saypolly
 // http://tigerfarmpress.com/cgi/echo.php
-// https://obedient-machine-3163.twil.io/echojson
-// https://obedient-machine-3163.twil.io/echojsonvalue
+// https://" . getenv('FUNCTIONS_HOST') . "/echojson
+// https://" . getenv('FUNCTIONS_HOST') . "/echojsonvalue
 $call = $client->calls->create(
         // Parameter order is important.
         $callTo,
         $callFrom,
         array(
-            "timeout" => "6",
-            "url" => "https://handler.twilio.com/twiml/EH10bd59478178c043b058b96142c69e06",
-            "StatusCallback" => "https://obedient-machine-3163.twil.io/echojsonvalue",
+            "url" => "https://" . getenv('FUNCTIONS_HOST') . "/saypolly",
+            // "timeout" => "6",
+            // "StatusCallback" => "https://" . getenv('FUNCTIONS_HOST') . "/echojsonvalue",
             // "statusCallbackMethod" => "POST", // POST GET
-            "StatusCallbackEvent" => array("initiated", "ringing", "answered", "completed")
+            // "StatusCallbackEvent" => array("initiated", "ringing", "answered", "completed")
             // "StatusCallbackEvent" => array("answered")
             )
 );
