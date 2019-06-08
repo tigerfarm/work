e319effb8d7fa3c1f898d5fa5fbbe673f9d8fb88
tigerfarm <tigerfarm@gmail.com>
2019-06-08 11:49:39 -0700

    updates

--- node/HttpPost.js
+++ node/HttpPost.js
@@ -1,30 +1,23 @@
 var request = require('request');
-//
-var ACCOUNT_SID = process.env.ACCOUNT_SID;
-var AUTH_TOKEN = process.env.AUTH_TOKEN;
-var basicAuth = "Basic " + new Buffer(ACCOUNT_SID + ":" + AUTH_TOKEN).toString("base64");
-//
-var theType = "json";
-var theRequestUrl = "https://api.twilio.com/2010-04-01/Accounts/" + ACCOUNT_SID + "/Messages." + theType;
-//
-console.log('+ URL request: ' + theRequestUrl + " : " + theFormData);
+// var basicAuth = "Basic " + new Buffer(process.env.ACCOUNT_SID + ":" + process.env.AUTH_TOKEN).toString("base64");
+var basicAuth = "Basic " + Buffer.from(process.env.ACCOUNT_SID + ":" + process.env.AUTH_TOKEN).toString("base64");
+var theRequestUrl = "https://api.twilio.com/2010-04-01/Accounts/" + process.env.ACCOUNT_SID + "/Messages." + "json";
+var theFormData = {
+    From: process.env.PHONE_NUMBER3,
+    To: process.env.PHONE_NUMBER4,
+    Body: 'Twilio support'
+};
+console.log('+ URL request: ' + theRequestUrl + " : " + JSON.stringify(theFormData));
 request({
-    // Options
     method: "POST",
     headers: {
         "Authorization": basicAuth,
         'content-type': 'application/x-www-form-urlencoded'
     },
     url: theRequestUrl,
-    formData: {
-        From: process.env.PHONE_NUMBER3,
-        To: process.env.PHONE_NUMBER4,
-        Body: 'Twilio support'
-    }},
-        function (error, response, body) {
-            // Call back
-            console.log(body);
-        }
-);
+    formData: theFormData
+}, function (error, response, body) {
+    console.log(body);
+});
 
 // eof
