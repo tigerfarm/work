b2a11c9213e8771d4272f3463a476393a9db8ad5
tigerfarm <tigerfarm@gmail.com>
2019-06-21 13:01:00 -0700

    updates

--- notify/sendToList3-args.py
+++ notify/sendToList3-args.py
@@ -3,22 +3,32 @@
 # ------------------------------------------------------------------------------
 print("+++ Send notifications.")
 import os
+import sys
 import json
 from twilio.rest import Client
 account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 #
-phoneNumber1 = os.environ.get("PHONE_NUMBER1")
-#
+# Note, first argument is the program name.
+print "+ NUmber of arguments = ", len(sys.argv)
+if len(sys.argv) < 3:
+    print "+ Requires arguments: to-phone-number message"
+    exit()
+if len(sys.argv) > 1:
+    print "+ 2nd argument:", sys.argv[1]
+if len(sys.argv) > 2:
+    print "+ 3rd argument:", sys.argv[2]
+print
+phoneNumber1 = sys.argv[1]
+theMessage = sys.argv[2]
 print("+ Send notifications to: " + phoneNumber1)
 print("+ Using notification service, SID: " + os.environ.get("NOTIFY_SERVICE_SID"))
 notify_service_sid = os.environ.get("NOTIFY_SERVICE_SID")
 notification = client.notify.services(notify_service_sid).notifications.create(
-    body='Hello to you two.',
+    body=theMessage,
     to_binding=[
-        json.dumps({"binding_type":"sms", "address": phoneNumber1}),
-        json.dumps({"binding_type":"sms", "address": os.environ.get("PHONE_NUMBER4")})
+        json.dumps({"binding_type":"sms", "address": phoneNumber1})
     ]
   )
 print("+ Notification SID: " + notification.sid)
