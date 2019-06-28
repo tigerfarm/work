86b6f4def02bc0744ec70616578759442963dffa
tigerfarm <tigerfarm@gmail.com>
2019-06-27 18:48:28 -0700

    updates

--- nodehttp/HttpGet.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-var theRequestUrl = 'http://localhost:8000/hello.txt?p1=abc&p2=def';
-console.log('+ URL request: ' + theRequestUrl);
-request({method: "GET", url: theRequestUrl},
-        function (error, response, body) {
-            console.log(body);
-        });
-
-// eof
