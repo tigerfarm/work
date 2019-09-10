1610bb125efd81d08c0f27a933b57aff0edb732b
tigerfarm <tigerfarm@gmail.com>
2019-09-10 12:28:47 -0700

    update

--- php/1-DateTests.php
+++ php/1-DateTests.php
@@ -3,23 +3,30 @@ echo "\xA+++ Date tests.\xA";
 //
 // http://php.net/manual/en/timezones.php
 //
+$theTimeZone = "America/Los_Angeles";
+date_default_timezone_set($theTimeZone);
+echo "+ Timezone: " . $theTimeZone;
+$now = new DateTime('now');
+echo "\xA+ now->format   : ". $now->format('Y-m-d H:i:s');
+echo "\xA";
+//
 date_default_timezone_set('UTC');
+//
 echo "\xA+ Date today UTC: " . date('Y-m-d');           // 2018-08-24
 echo "\xA+ Time          : " . date("H:i:sa");
 date_default_timezone_set('America/Los_Angeles');
 echo "\xA+ Date today PDT: " . date('Y-m-d');           // 2018-08-24
 echo "\xA+ Time          : " . date("H:i:sa");
 echo "\xA= Sample date   : " . date('l jS \of F Y h:i:s A');
-echo "\xA\xA";
+echo "\xA";
+//
 echo "\xA+ Date today PDT: " . date('Y-m-d H:i:sa');    // 2018-08-24
 $nowStamp = mktime(0,0,0,date("m"),date("d"),date("Y"));
 echo "\xA+ nowStamp->format   : ". $nowStamp;                     // Timestamp: 1535353200
-$now = new DateTime('now');
-echo "\xA+ now->format   : ". $now->format('Y-m-d H:i:s');
 //
 $date = new DateTime('2001-02-03 04:05:06');
 echo "\xA+ date->format  : ". $date->format('Y-m-d H:i:s');
-echo "\xA\xA";
+echo "\xA";
 
 $todayStart = date('Y-m-d') + ' 00:00:00';  // date('2018-08-24 00:00:00')
 $date1 = new DateTime($todayStart);
