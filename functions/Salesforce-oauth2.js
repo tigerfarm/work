f493242540b862429270f40b4af4e32655374735
tigerfarm <tigerfarm@gmail.com>
2020-03-12 18:01:18 -0700

    updates

---
+++ functions/Salesforce-oauth2.js
@@ -0,0 +1,23 @@
+exports.handler = function(context, event, callback) {
+    console.log("Log message.");
+    var theRequest = "https://test.salesforce.com/services/oauth2/token";
+    console.log('+ theRequest: ' + theRequest);
+    got.post(theRequest, {
+        body: {
+            grant_type: 'password',
+            client_id: clientId,
+            client_secret: clientSecret,
+            username: sfUserName,
+            password: sfPassword
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(function (response) {
+        console.log("+ Salesforce response: " + response.body);
+        callback("+ Message posted.");
+    }).catch(function (error) {
+        console.log(error);
+        callback("- Error posting message : " + error);
+    });
+};
