befbcbfc658c0660bbec69a5b37425f8e5f88bc5
tigerfarm <tigerfarm@gmail.com>
2020-04-16 11:44:25 -0700

    updates

--- php/smsSend.php
+++ php/smsSend.php
@@ -3,14 +3,18 @@ require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
 use Twilio\Rest\Client;
 // $client = new Client(getenv('ACCOUNT_SID'), getenv('AUTH_TOKEN'));
 $client = new Client(getenv('MASTER_ACCOUNT_SID'), getenv('MASTER_AUTH_TOKEN'));
+// $client = new Client(getenv('LABS_ACCOUNT_SID'), getenv('LABS_AUTH_TOKEN'));
+//
+// $fromPhoneNumber = "david";
+// $toPhoneNumber = getenv('MASTER_UK');
+//
 // $fromPhoneNumber = getenv('PHONE_NUMBER4');
-$fromPhoneNumber = "david";
 // $toPhoneNumber = getenv('PHONE_NUMBER3');
 // $toPhoneNumber = getenv('MY_PHONE_NUMBER');
-$toPhoneNumber = getenv('MASTER_UK');
+$toPhoneNumber = getenv('MASTER_PHONE_NUMBER_1');
 //
 // $theMessageSupport = "Twilio support test \nmessage #3, \n newline.";
-$theMessageSupport = "Twilio support test #1";
+$theMessageSupport = "Twilio support test #2";
 $theMessageChinese = "你好";
 $theMessageOkay = "okay 4";
 $theMessage = $theMessageSupport;
