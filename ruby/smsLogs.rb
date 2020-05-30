e680d0d59b9fa39883c9b989ec613cdbf1537afd
tigerfarm <tigerfarm@gmail.com>
2020-05-29 17:38:04 -0700

    updates

--- ruby/smsLogs.rb
+++ ruby/smsLogs.rb
@@ -7,9 +7,11 @@ auth_token = ENV["MASTER_AUTH_TOKEN"]
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 messages = @client.messages.list(
     date_sent_after: Time.new(2020, 5, 21, 16, 0, 0),
-    date_sent_before: Time.new(2020, 5, 21, 24, 0, 0),
-    limit: 10000
+    date_sent_before: Time.new(2020, 5, 21, 24, 0, 0)
 )
+# Optional, in the above, add property limit, to limit the number of rows retrieved. For example:
+#   limit: 200
+#
 # List of properties for output:
 # https://www.twilio.com/docs/sms/api/message-resource#message-properties
 messages.each do |record|
