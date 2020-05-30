77d313ab69467c3b10102ad2f6c3af4a8d2f2703
tigerfarm <tigerfarm@gmail.com>
2020-05-29 17:32:59 -0700

    updates

--- ruby/smsLogs.rb
+++ ruby/smsLogs.rb
@@ -1,12 +1,14 @@
+# https://www.twilio.com/docs/sms/api/message-resource?code-sample=code-read-list-messages-filter-by-a-period-of-time&code-language=Ruby&code-sdk-version=5.x#read-multiple-message-resources
+# 
 require 'rubygems'
 require 'twilio-ruby'
 account_sid = ENV["MASTER_ACCOUNT_SID"]
 auth_token = ENV["MASTER_AUTH_TOKEN"]
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 messages = @client.messages.list(
-    date_sent_after: Time.new(2020, 5, 21, 21, 0, 0),
-    date_sent_before: Time.new(2020, 5, 21, 22, 0, 0),
-    limit: 20
+    date_sent_after: Time.new(2020, 5, 21, 16, 0, 0),
+    date_sent_before: Time.new(2020, 5, 21, 24, 0, 0),
+    limit: 10000
 )
 # List of properties for output:
 # https://www.twilio.com/docs/sms/api/message-resource#message-properties
