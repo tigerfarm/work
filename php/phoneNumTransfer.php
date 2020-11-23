265f3ea46fd231094e4a169b78085da803e7a218
tigerfarm <tigerfarm@gmail.com>
2020-11-23 10:48:24 -0800

    updates

--- php/phoneNumTransfer.php
+++ php/phoneNumTransfer.php
@@ -5,11 +5,11 @@ require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
 
 use Twilio\Rest\Client;
 // $twilio = new Client(getenv("ACCOUNT_SID"), getenv('AUTH_TOKEN'));
-$twilio = new Client("ACff2d41e2d2d41acaa626167b8e63bfcd", "c948de3e0d4f7d2f86a68ae7acbd7120");
+$twilio = new Client("ACf...d", "c...0");
 
-$incoming_phone_number = $twilio->incomingPhoneNumbers("PN7e0ed6147a88788e989377b7043f5a9e")
+$incoming_phone_number = $twilio->incomingPhoneNumbers("PN7e...e")
         ->update(array(
-    "accountSid" => "ACae0e356ccba96d16d8d4f6f9518684a3"
+    "accountSid" => "ACa...3"
         )
 );
 echo "\xA++ Moved\xA";
