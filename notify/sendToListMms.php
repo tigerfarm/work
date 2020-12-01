759d905d5a5708e436f1d9bdbb481c16a44f285a
tigerfarm <tigerfarm@gmail.com>
2020-11-30 17:08:56 -0800

    updates

--- notify/sendToListMms.php
+++ notify/sendToListMms.php
@@ -1,5 +1,5 @@
 <?php
-
+// NodeJS it looks like this: sms: { "media_urls": ['imageaddress.com/image.png'] }
 require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
 
 use Twilio\Rest\Client;
@@ -9,9 +9,7 @@ $notification = $twilio->notify->v1->services(getenv('MASTER_NOTIFY_SERVICE_SID'
         ->notifications
         ->create( array(
             "body" => "hello 2.1",
-            "sms" => [
-                "media_urls" => ["https://www.tigerfarmpress.com/StacyDavid/netscape-fountain2a.jpg"]
-            ],
+            "sms" => ["media_urls" => ["https://www.tigerfarmpress.com/StacyDavid/netscape-fountain2a.jpg"]],
             "toBinding" => array(
                 "{\"binding_type\":\"sms\", \"address\":\"" . getenv('MY_PHONE_NUMBER') . "\"}"
             )
