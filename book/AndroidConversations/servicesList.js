d2b088c1049b1defa5d9c37c15ced8e4fec98e3b
Stacy David Thurston <tigerfarm@gmail.com>
2021-07-29 09:14:48 -0700

    update

---
+++ book/AndroidConversations/servicesList.js
@@ -0,0 +1,10 @@
+console.log("++ List Services.");
+var client = require('twilio')(process.env.MASTER_ACCOUNT_SID, process.env.MASTER_AUTH_TOKEN);
+
+client.conversations.services.list({limit: 20})
+        .then(services => services.forEach(s =>
+                console.log(
+                        "+ Service SID: " + s.sid
+                        + " " + s.friendlyName
+                        )));
+      
