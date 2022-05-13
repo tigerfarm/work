73bae29b473f4fe3c6ee0ebdb94339973610823f
Stacy David Thurston <tigerfarm@gmail.com>
2022-05-13 14:55:58 -0700

    Update

--- php/phoneNumList1.php
+++ php/phoneNumList1.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting( E_ALL ^ ( E_NOTICE | E_WARNING | E_DEPRECATED ) );
 require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
 use Twilio\Rest\Client;
 $twilio = new Client(getenv('MASTER_ACCOUNT_SID'), getenv('MASTER_AUTH_TOKEN'));
---
+++ php/phoneNumUpdate.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL ^ ( E_NOTICE | E_WARNING | E_DEPRECATED ));
+require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
+use Twilio\Rest\Client;
+$twilio = new Client(getenv('MASTER_ACCOUNT_SID'), getenv('MASTER_AUTH_TOKEN'));
+$pnSid = "PN915869791137c6f05b12f4eb2e23a704";
+print("\xA+ Print Phone numbers information: " . $pnSid . "\xA");
+
+// $pnupdate = $twilio->incomingPhoneNumbers($pnSid)->update(["statusCallback" => "https://tfpecho.herokuapp.com/wh"]);
+$pnupdate = $twilio->incomingPhoneNumbers($pnSid)->update(["statusCallback" => null]);
+    
+// Properties: https://www.twilio.com/docs/phone-numbers/api/incomingphonenumber-resource#incomingphonenumber-properties
+$pnfetch = $twilio->incomingPhoneNumbers($pnSid)
+        ->fetch();
+    echo $pnfetch->dateCreated->format('Y-m-d')
+    . ", " . $pnfetch->sid
+    . ", " . $pnfetch->phoneNumber
+    . ", " . $pnfetch->friendlyName
+    . ", " . $pnfetch->statusCallback
+    . "\xA";
+
+echo "\xA+ End of lists.\xA";
+?>
