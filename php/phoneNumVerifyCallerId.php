5ad895ed14a294f9fae4ed48f50267a13ec2ae0c
Stacy David Thurston <tigerfarm@gmail.com>
2022-06-29 17:44:33 -0700

    update

--- php/phoneNumVerifyCallerId.php
+++ php/phoneNumVerifyCallerId.php
@@ -1,8 +1,8 @@
 <?php
 // Docs: https://www.twilio.com/docs/voice/api/outgoing-caller-ids#add-an-outgoing-caller-id
-// When this runs, a 6 digit validation code is returned.
-// Twilio willphone the phone number and ask for the validation code.
-// Once the called person enters the code, the phone number becomes verified and can be used as a caller id.
+// When this program runs, it returns a 6 digit validation code.
+// Twilio phoned the phone number and asked for the validation code.
+// Once I entered the code, the phone number became verified and it can be used as a caller id.
 
 error_reporting( E_ALL ^ ( E_NOTICE | E_WARNING | E_DEPRECATED ) );
 require __DIR__ . '/../../twilio-php-master/Twilio/autoload.php';
