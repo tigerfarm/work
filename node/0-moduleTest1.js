864139ad865080d929054392c3c47a1093f8c4d4
Stacy David Thurston <tigerfarm@gmail.com>
2022-10-19 10:43:00 -0700

    update

--- node/0-moduleTest1.js
+++ node/0-moduleTest1.js
@@ -1,9 +1,14 @@
 // Module to be loaded into a node program.
 //      require('./0-moduleTest1.js');
 //
-var aNumber = 9; // PI will not be accessible from outside this module
+var internalNumber = 1; // This variable only accessible in this module.
+
 exports.doAdd = function (x,y) {
   return x + y;
 };
 
+// Only "exports" values are accessiable from other programs that "require(...)" this module.
 exports.aNumber = 12;
+exports.bNumber = internalNumber;
+
+// eof
--- node/0-moduleTest1run.js
+++ node/0-moduleTest1run.js
@@ -3,4 +3,5 @@ console.log( '+ Load module.');
 var circle = require('./0-moduleTest1.js');
 console.log( '++ 3 + 6 = ' + circle.doAdd(3,6));
 console.log( '++ exports.aNumber = ' + circle.aNumber);
+console.log( '++ exports.bNumber = ' + circle.bNumber);
 
