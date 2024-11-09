1c5932e170a8b83b083f716d6c9b25d7c1ea348a
Stacy David Thurston <tigerfarm@gmail.com>
2024-11-08 18:01:12 -0800

    update

--- a2p/endUserFetch.js
+++ a2p/endUserFetch.js
@@ -1,19 +1,18 @@
-console.log("++ Create EndUser of type: authorized_representative_1.");
+console.log("++ Fetch and list data for an EndUser.");
 var client = require('twilio')(process.env.MAIN_ACCOUNT_SID, process.env.MAIN_AUTH_TOKEN);
 async function runFunction() {
-    const endUser = await client.trusthub.v1.endUsers.create({
-        attributes: {
-            // first_name: "rep1",
-            // last_name: "acme",
-            job_position: "CEO",
-            phone_number: "+11234567890",
-            email: "rep1@acme.com",
-            business_title: "ceo",
-        },
-        friendlyName: "auth_rep_1",
-        type: "authorized_representative_1",
-    });
-    console.log("+ SID: " + endUser.sid);
+
+    const endUser = await client.trusthub.v1
+            .endUsers("IT82ce2d033753e5e6bc38930f0b18447d")
+            .fetch();
+    console.log(
+            "+ SID: " + endUser.sid 
+            + " friendlyName: " + endUser.friendlyName
+            + " type: " + endUser.type
+            );
+    console.log(
+            "+ attributes: " + JSON.stringify(endUser.attributes) 
+            );
 }
 runFunction().catch(function (err) {
     console.error("- Error: " + err.message + ", code: " + err.code);
