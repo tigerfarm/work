081950aa2e665e3876bfd813dbdd640b3f16a3ba
Stacy David Thurston <tigerfarm@gmail.com>
2025-02-12 13:52:21 -0800

    update

---
+++ a2p/listCampaign.js
@@ -0,0 +1,22 @@
+console.log("++ List a campaign's information.");
+// https://www.twilio.com/docs/messaging/api/usapptoperson-resource#code-fetch-a-us-a2p-campaign
+//
+var client = require('twilio')(process.env.MAIN_ACCOUNT_SID, process.env.MAIN_AUTH_TOKEN);
+async function runFunction() {
+    console.log("+ Usa2p resource info:");
+
+    const usAppToPerson = await client.messaging.v1
+    .services("MG8602f0e431cba458ff299d5e2722b785")
+    .usAppToPerson("QE2c6890da8086d771620e9b13fadeba0b")    // What is QE code?
+    .fetch();
+
+  console.log("++ " + usAppToPerson.sid 
+          + "\n description:          " + usAppToPerson.description
+          + "\n usAppToPersonUsecase: " + usAppToPerson.usAppToPersonUsecase
+          + "\n campaignStatus:       " + usAppToPerson.campaignStatus
+          );    
+}
+runFunction().catch(function (err) {
+    console.error("- Error: " + err.message + ", code: " + err.code);
+    console.log("--- Exit.");
+});
